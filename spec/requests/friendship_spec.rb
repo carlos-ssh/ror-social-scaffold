0fc8889fa6a6dbc0c37d82f848e5a1b09a53baa2
Carlos-SSH <carlos.anezeh@me.com>
2021-01-26 14:09:51 -0600

    Add tests to spec file.

---
+++ spec/requests/friendship_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.describe 'Friendship requests' do
+  let(:create_users) do
+    User.create(name: 'carlos', email: 'carlos@carlos.com', password: 'omar')
+    User.create(name: 'omar', email: 'omar@omar.com', password: 'omar')
+  end
+
+  describe 'POST /create' do
+    it 'creates a friendship request and get a redirect status' do
+      create_users
+      friendship_params = {
+        user_id: User.first.id,
+        friend_id: User.last.id
+      }
+      post user_friendships_path(User.first), params: friendship_params
+      expect(response).to have_http_status(302)
+    end
+  end
+end
